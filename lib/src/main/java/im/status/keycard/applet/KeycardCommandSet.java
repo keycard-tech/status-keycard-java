e898afdb21b2066f0f4a1b8cd8cba733f5370ac1
Michele Balistreri <michele@bitgamma.com>
2026-02-25 16:23:57 +0100

    remove wrong algorithm selection

--- lib/src/main/java/im/status/keycard/applet/KeycardCommandSet.java
+++ lib/src/main/java/im/status/keycard/applet/KeycardCommandSet.java
@@ -585,7 +585,7 @@ public class KeycardCommandSet {
    * @throws IOException communication error
    */
   public APDUResponse sign(byte[] data, int p1) throws IOException {
-    APDUCommand sign = secureChannel.protectedCommand(0x80, INS_SIGN, p1, 0x01, data);
+    APDUCommand sign = secureChannel.protectedCommand(0x80, INS_SIGN, p1, 0x00, data);
     return secureChannel.transmit(apduChannel, sign);
   }
 
